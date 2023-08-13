ae6a4a5554f03cee51bf0e7367645d6703573fcf
manvisingh3097 <singh.thakurmanvi@gmail.com>
2023-08-14 02:59:37 +0530

    added navbar items

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -42,6 +42,37 @@ const Navbar = () => {
                 </span>
             </button>
 
+            <div className=" navbar navbar-expand-lg navbar-light bg-light container " id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto navbar container">
+                    
+                        <li className="nav-item active ">
+                        <Link className="nav-link" to="/">
+                            HOME
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/cat/ABOUT">
+                           ABOUT
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/cat/FACILITIES">
+                        FACILITIES
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/cat/BLOG">
+                            BLOG
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/cat/CONTACT">
+                            CONTACT
+                        </Link>
+                    </li>
+                </ul>
+                </div>
+
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav ml-auto">
                     {loginStatus ? (
